8b4eed800d8d51b440fabd701f21a8c2fb10ac29
danashkol <danashkol@gmail.com>
2022-07-04 12:11:00 +0300

    dana

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def interact_db(query, query_type: str):
     return_value = False
     connection = mysql.connector.connect(host='localhost',
                                          user='root',
-                                         passwd='Arbesfeld@123',
+                                         passwd='Root',
                                          database='project_db')
     cursor = connection.cursor(named_tuple=True)
     cursor.execute(query)
@@ -41,10 +41,60 @@ from pages.payment.payment import payment
 app.register_blueprint(payment)
 
 
-@app.route('/')
-def index_func():
-    return render_template('home page.html')
 
 app.secret_key = '123'
 app.config['SESSION_PERMANENT'] = True
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=1000)
+
+@app.route('/')
+def homePage():
+    return render_template('home page.html')
+
+
+@app.route('/girlCakes')
+def girlCakes():
+    return render_template('girl cakes.html')
+
+@app.route('/boyCakes')
+def boyCakes():
+    return render_template('boy cakes.html')
+
+@app.route('/adultCakes')
+def adultCakes():
+    return render_template('adult cakes.html')
+
+@app.route('/numLetterCakes')
+def numLetCakes():
+    return render_template('num _ letter cakes.html')
+
+@app.route('/weddingCakes')
+def weddingCakes():
+    return render_template('wedding cakes.HTML')
+
+@app.route('/ringCakes')
+def ringCakes():
+    return render_template('ring cakes.html')
+
+@app.route('/bachPartyCakes')
+def bachPartyCakes():
+    return render_template('bachlorette party cakes.html')
+
+@app.route('/pies')
+def pies():
+    return render_template('pies.html')
+
+@app.route('/yeastCakes')
+def yeastCakes():
+    return render_template('yeast cakes.html')
+
+@app.route('/specialCakes')
+def specialCakes():
+    return render_template('special cakes.html')
+
+@app.route('/contactUs')
+def contactUs():
+    return render_template('Contact Us.html')
+
+
+if __name__ == '__main__':
+    app.run()
--- costumers.sql
+++ costumers.sql
@@ -1,3 +1,16 @@
+CREATE TABLE costumers (
+
+username varchar(255) primary key ,
+
+fullName varchar(255),
+
+email text,
+
+password varchar(120)
+
+);
+
+
 INSERT INTO project_db.costumers (username, fullName, email, password) VALUES ('Dana', 'sds', 'ccsd@dsf.zcs', '1');
 INSERT INTO project_db.costumers (username, fullName, email, password) VALUES ('Dana1', 'dana shkol', 'sdcfsn@sdcs.xqw', '1');
 INSERT INTO project_db.costumers (username, fullName, email, password) VALUES ('Noam1', 'noam a', 'no@sdf.com', '111');
